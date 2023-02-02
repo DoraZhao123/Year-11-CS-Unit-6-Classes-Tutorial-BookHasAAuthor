50d77ada618b5bfde2d11a34b321f9a48581732f
dorazhao <doraz2025@student.cis.edu.hk>
2023-02-02 14:31:33 +0800

    BookHasAAuthor

---
+++ src/Book.java
@@ -0,0 +1,47 @@
+public class Book {
+    private String name;
+    private Author author;
+    private double price;
+    private int qty;
+
+    public Book(String name, Author author, double price, int qty){
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Author getAuthor(){
+        return author;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public int getQty(){
+        return qty;
+    }
+
+    public void setQty(int qty){
+        this.qty = qty;
+    }
+
+    public String toString(){
+        return "'" + name + "'" + " by " + author;
+    }
+
+//    public static void main(String[] args) {
+//        Book book = new Book("The Man in the High Castle", author, 100, 10);
+//        System.out.println(book);
+//    }
+
+}
